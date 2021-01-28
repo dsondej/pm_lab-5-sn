5fb895ed9abe2d7412d45e4de86c2c7878778a8c
Patryk <patryk.lelakowski@student.wat.edu.pl>
2021-01-28 19:19:00 +0100

    Wykonano  zadanie 3.2.6 -sterowanie silnikiem DC

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,14 +1,11 @@
-
-#include <Arduino.h>
 #include <LiquidCrystal.h>
-
-#define buttonUp    11
-#define buttonDown  13
-#define buttonOK    12
 #define LedRed 3
 #define LedGreen 1
-#define LedBlue 2 
-
+#define LedBlue 2
+#define buttonUp    11
+#define buttonDown  13
+#define buttonOK  12
+#define went 10
 LiquidCrystal lcd(9, 8, 7, 6, 5, 4);
 int menu=1;
 bool psButtonUp = LOW;
@@ -38,22 +35,17 @@ void changeMenu(void){
   psButtonDown = digitalRead(buttonDown);
 }
 
-void changeRGBLed(void){
-  float change = (temperature+40.0f) * 255.0f / (125.0f+40.0f);
-  analogWrite(LedRed, 0+change);
-  analogWrite(LedGreen,0);
-  analogWrite(LedBlue, 255-change);
-}
-
-
 void setup(void){
   lcd.begin(16, 2);
   pinMode(buttonUp, INPUT_PULLUP);
-  pinMode(buttonOK, INPUT_PULLUP);
   pinMode(buttonDown, INPUT_PULLUP);
+  pinMode(buttonOK, INPUT_PULLUP);
   pinMode(LedRed, OUTPUT);
   pinMode(LedGreen, OUTPUT);
   pinMode(LedBlue, OUTPUT);
+  pinMode(went,OUTPUT);
+  analogWrite(went,0);
+  Serial.begin(9600);
 }
 
 void loop(void) {
@@ -61,13 +53,33 @@ void loop(void) {
   changeMenu();
   readTemperature();
   changeRGBLed();
+  runFAN();
 }
-
+void changeRGBLed(void){
+  float change = (temperature + 40.0f) * 255.0f / (125.0f + 40.0f);
+  analogWrite(LedRed, 0+change);
+  analogWrite(LedGreen, 0);
+  analogWrite(LedBlue, 255+change);
+}
+  
 void readTemperature(void){
   unsigned int digital = analogRead(A5);
   float resolution = (5.0f / 1024.0f);
   float voltage = resolution * digital;
   temperature = (voltage-0.1f) * (125.0f+40.0f) / (1.75f-0.1f) - 40.0f;
+}
+
+void runFAN(void){
+if(temperature>40)
+{
+  float duty = map(temperature,40,127,10,255);
+  analogWrite(went, duty);
+  Serial.println(duty);
+
+  Serial.println(temperature);
+
+}
+  else analogWrite(went, 0);
 }
 
 void dispMenu(void){
